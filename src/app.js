b7d961c93354035409c822c3f46924d8fd683368
CjSystem2030 <cristian_56_90@hotmail.com>
2019-12-25 15:59:34 -0500

    first commit

---
+++ src/app.js
@@ -0,0 +1,6 @@
+import "./styles/main.scss";
+import "./styles/about.scss";
+
+import "./static/logo.png";
+import "./static/nave.png";
+import "./static/bigdata.png";
---
+++ webpack/webpack.config.js
@@ -0,0 +1,115 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const autoprefixer = require('autoprefixer');
+
+module.exports = {
+    entry: {
+        index:'./src/app.js',
+        about:'./src/about.js'
+        
+    },
+    output: {
+        path: path.resolve(__dirname, '../dist'),
+        filename: 'js/[name].bundle.js'
+    },
+    devtool: 'source-map',
+    devServer: {
+        port: 4000,
+        open: true
+    },
+    module: {
+        rules: [
+            {
+                test: /\.handlebars$/,
+                loader: 'handlebars-loader'
+            },
+            {
+                test: /\.(sa|sc|c)ss$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    {
+                        loader: 'postcss-loader',
+                        options: {
+                            autoprefixer: {
+                                browser: ["last 2 versions"]
+                            },
+                            plugins: () => [
+                                autoprefixer
+                            ]
+                        }
+                    },
+                    'sass-loader',
+                ],
+            },
+            {
+                test: /\.(jpg|png|gif)$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: '[name].[ext]',
+                            outputPath: 'static/',
+                            useRelativePath: true
+                        }
+                    },
+                    {
+                        loader: 'image-webpack-loader',
+                        options: {
+                            mozjpeg: {
+                                progressive: true,
+                                quality: 65
+                            },
+                            // optipng.enabled: false will disable optipng
+                            optipng: {
+                                enabled: false,
+                            },
+                            pngquant: {
+                                quality: '65-90',
+                                speed: 4
+                            },
+                            gifsicle: {
+                                interlaced: false,
+                            },
+                            // the webp option will enable WEBP
+                            webp: {
+                                quality: 75
+                            }
+                        }
+                    }
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            filename:'index.html',
+            template:'./src/index.handlebars',
+            chunks:['index'],
+            minify: {
+                html5: true,
+                collapseWhitespace: true,
+                caseSensitive: true,
+                removeComments: true,
+                removeEmptyElements: true
+            }
+        }), 
+        new HtmlWebpackPlugin({
+            filename:'about.html',
+            template:'./src/about.handlebars', 
+            chunks:['about'],       
+            minify: {
+                html5: true,
+                collapseWhitespace: true,
+                caseSensitive: true,
+                removeComments: true,
+                removeEmptyElements: true
+            }
+        }),
+        new MiniCssExtractPlugin({
+            filename: "css/[name]-styles.css",
+            chunkFilename: "[id].css"
+        })
+    ]
+};
